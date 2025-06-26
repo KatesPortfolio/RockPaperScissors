8e5fdd96d38b2c24a79a57959c9311986db7a3be
KatarinaDojcinovska <katarina.dojcinovska98@gmail.com>
2025-06-26 19:23:04 +0200

    Function getComputerChoice works
    The computer generates random from the array

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 console.log("Hello world")
+
+function getComputerChoice (){
+    let array = ["rock", "paper", "scissors"];
+    const randomNumber = Math.floor(Math.random() * array.length);
+
+    return array[randomNumber]
+}
+
+console.log(getComputerChoice())
