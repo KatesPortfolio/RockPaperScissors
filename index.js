518997355f8e96facf0c4f3fc2278d1c79850ecb
KatarinaDojcinovska <katarina.dojcinovska98@gmail.com>
2025-06-28 14:01:41 +0200

    Changed the function playRound(humanChoice, computerChoice)
    I needed to use return humanChoice and return computerChoice to be able to "save" the winner and return it.

--- index.js
+++ index.js
@@ -1,21 +1,35 @@
-console.log("Hello world")
+console.log("Hello world");
 
-function getComputerChoice (){
-    let array = ["rock", "paper", "scissors"];
-    const randomNumber = Math.floor(Math.random() * array.length);
+function getComputerChoice() {
+  let array = ["rock", "paper", "scissors"];
+  const randomNumber = Math.floor(Math.random() * array.length);
 
-    return array[randomNumber]
+  return array[randomNumber];
 }
 
-console.log(getComputerChoice())
-
-function getHumanChoice (){
-    const playerChoice = prompt("Write rock, paper, scissors");
-
-    return playerChoice;
+function getHumanChoice() {
+  const playerChoice = prompt("Write rock, paper, scissors");
+  return playerChoice;
 }
 
-console.log(getHumanChoice())
+}
 
-let humanScore = 0;
-let computerScore = 0;
+function playGame() {
+  let humanScore = 0;
+  let computerScore = 0;
+
+  for (let i = 0; i < 5; i++) {
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+
+    const player = playRound(humanSelection, computerSelection);
+
+    if(player === humanSelection){
+      humanScore++
+    } else if (player === computerSelection){
+      computerScore++
+    }
+  }
+  console.log(`Human ${humanScore} Computer ${computerScore}`);
+}
+playGame();
